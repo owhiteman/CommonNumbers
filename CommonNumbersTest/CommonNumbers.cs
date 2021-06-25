e69450a8e95a90bda22730b03a719deaf3730b46
owhiteman <ollie.whiteman@gmail.com>
2021-06-25 12:28:46 +1200

    Initial check-in of module CommonNumbersTest

---
+++ CommonNumbersTest/CommonNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonNumbersTest
+{
+    public class CommonNumbers
+    {
+        public CommonNumbers()
+        {
+        }
+
+
+        public List<string> CreateList(string[] list1, string[] list2)
+        {
+            List<string> commonList = new List<string>();
+            commonList.Add(FindCommon(list1));
+            commonList.Add(FindCommon(list2));
+
+            return commonList;
+        }
+
+        public string FindCommon(string[] list)
+        {
+            string commonNumbers = "";
+            string split1 = list[0].Replace(" ", "");
+            List<string> string1 = split1.Split(',').ToList();
+
+            string split2 = list[1].Replace(" ", "");
+            List<string> string2 = split2.Split(',').ToList();
+
+            foreach (string i in string1)
+            {
+                foreach (string j in string2)
+                {
+                    if (i.Equals(j))
+                    {
+                        commonNumbers += i + ",";
+                    }
+                }
+            }
+
+            return commonNumbers.TrimEnd(',');
+        }
+    }
+}
---
+++ CommonNumbersTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonNumbersTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        CommonNumbers common = new CommonNumbers();
+        string[] testArray1 = { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" };
+        string[] testArray2 = { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" };
+
+        [TestMethod]
+        public void TestFindCommonMethod()
+        {
+            string expected = "1,4,13";
+
+            Assert.AreEqual(expected, common.FindCommon(testArray1));
+
+        }
+
+        [TestMethod]
+        public void TestFindCommonMethod2()
+        {
+            string expected = "1,9,10";
+
+            Assert.AreEqual(expected, common.FindCommon(testArray2));
+
+        }
+
+        [TestMethod]
+        public void TestCreateList()
+        {           
+            List<string> expected = new List<string> { "1,4,13", "1,9,10" };
+
+            Assert.AreEqual(expected.ToString(), common.CreateList(testArray1, testArray2).ToString());
+
+        }
+    }
+}
